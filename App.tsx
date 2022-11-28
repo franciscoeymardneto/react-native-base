62fd78eb16ba0f16abc879c4bf76eefb7643fae2
netoeymard <eymard@sysautomacao.com.br>
2022-11-28 11:32:48 -0300

    chore: add expo fonts dependences

--- App.tsx
+++ App.tsx
@@ -1,18 +1,34 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback } from 'react';
 import { StyleSheet } from 'react-native';
 import Routes from './src/navigation/routes';
+import { FONTS } from "./utils/constants";
 
 export default function App() {
+  const [fontsLoaded] = useFonts(FONTS)
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Routes/>
+      <Routes/>
     
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
