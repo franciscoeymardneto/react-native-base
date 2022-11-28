f0cef0b2116729e0a130b25c45208b1e88a029a2
netoeymard <eymard@sysautomacao.com.br>
2022-11-28 11:32:28 -0300

    config: add fonts constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,3 +3,15 @@ import {
 } from "react-native";
 
 export const SCREEN_WIDTH = Dimensions.get("window").width;
+
+
+export const FONTS = {
+    "ubuntu-bold": require("../assets/fonts/Ubuntu-Bold.ttf"),
+    "ubuntu-bold-italic": require("../assets/fonts/Ubuntu-BoldItalic.ttf"),
+    "ubuntu-italic": require("../assets/fonts/Ubuntu-Italic.ttf"),
+    "ubuntu-light": require("../assets/fonts/Ubuntu-Light.ttf"),
+    "ubuntu-light-italic": require("../assets/fonts/Ubuntu-LightItalic.ttf"),
+    "ubuntu-medium": require("../assets/fonts/Ubuntu-Medium.ttf"),
+    "ubuntu-medium-italic": require("../assets/fonts/Ubuntu-MediumItalic.ttf"),
+    "ubuntu-regular": require("../assets/fonts/Ubuntu-Regular.ttf")
+  }
